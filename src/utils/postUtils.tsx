d90e524ed3011609344533c7b991878f4670e235
Adarsh Acharya <adarshsohamacharya888@gmail.com>
2024-10-19 16:27:59 +0530

    careers page added

--- src/utils/postUtils.tsx
+++ src/utils/postUtils.tsx
@@ -1,12 +1,38 @@
 export const getPostColorFromCategory = (category: string) => {
+  switch (category) {
+    case "Sustainability":
+      return "lime";
+    case "Innovation":
+      return "cyan";
+    case "Security":
+      return "violet";
+    default:
+      return "fucshia";
+  }
+};
+
+export const getColorFromPositionType = (type: string) => {
+  switch (type) {
+    case "Full Time":
+      return "lime";
+    case "Part Time":
+      return "cyan";
+    case "Contract":
+      return "violet";
+    default:
+      return "fuchsia";
+  }
+};
+
+export const getColorFromPositionCategory = (category: string) => {
     switch(category){
-        case "Sustainability":
-            return "lime"
-        case "Innovation":
-            return "cyan"
-        case "Security":
-            return "violet"
+        case "Engineering":
+            return "lime";
+        case 'Product':
+            return 'cyan'
+        case 'Marketing':
+            return 'violet'
         default:
-            return "fucshia"
+            return 'fuchsia'
     }
-}
+};
